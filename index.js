45b2099b1126c52844460f0c5f2cf113fec29bf6
dogoking <dogo.kingsley@gmail.com>
2022-10-01 20:45:19 +0100

    Adding HTML, CSS and JS files with code

---
+++ index.js
@@ -0,0 +1,10 @@
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+
+console.log(num1)
+console.log(num2)
+
+
+const questionEl = document.getElementById("question");
+
+
